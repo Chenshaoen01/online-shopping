3255c834f1cb9795f3f9c914b2d09a3e0f603df9
SeanChen <chenshaoen01@gmail.com>
2025-01-04 00:59:59 +0800

    modify footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 export default () => {
-    return <div className="footer">
-        <div className="flex">
-            <img className="footer-logo me-2" src="/logo1.png"></img>
-            <span>毛孩物坊</span>
+    return <div className="footer flex flex-col items-center">
+        <div className="flex flex-col items-center mb-2">
+            <img className="footer-logo me-2 mb-2" src="/logo1.png"></img>
+            <span className="text-xl font-bold">毛孩物坊</span>
         </div>
-        <div className="flex flex-col md:flex-row text-center">
+        <div>信箱：chenshaoen01@gmail.com</div>
+        <div>連絡電話：0925636386</div>
+        {/* <div className="flex flex-col md:flex-row text-center">
             <span className="mx-4">本網站僅為個人作品，無商業用途</span>
             <span>Copyright © 2024 陳劭恩 .All rights reserved.</span>
-        </div>
+        </div> */}
     </div>
 }
