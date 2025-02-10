9eb80b23152d01424e976312c5f47b3dd780b04f
SeanChen <chenshaoen01@gmail.com>
2025-02-11 00:08:42 +0800

    modify google-site-verification meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,16 +2,19 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import "./alertify.min.css";
 import "./micromodal.css";
+import Head from "next/head";
 
 export const metadata = {
   title: "毛孩物坊",
-  description: "毛孩物坊",
-  "google-site-verification": "x4YtdS4stjAmVmjIPiwBnI9EI07fimDdQE8wHTtAE-c"
+  description: "毛孩物坊"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+          <meta name="google-site-verification" content="x4YtdS4stjAmVmjIPiwBnI9EI07fimDdQE8wHTtAE-c" />
+      </Head>
       <body className="main-body">
         {children}
         <Footer></Footer>
